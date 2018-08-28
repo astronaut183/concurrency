81541afd35ba9297ff0c93930b79d4e6b9e1cf2e
astronaut183 <yzhan113@stevens.edu>
2018-08-28 00:18:29 -0400

    concurrency

---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,16 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程里用来标记线程不安全的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
